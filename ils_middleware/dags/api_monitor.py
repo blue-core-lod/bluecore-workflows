8f692926cb0022f3d53a59361d634a2224840ed9
Jeremy Nelson <jermnelson@gmail.com>
2025-02-20 13:20:52 -0800

    refactor: Changes DAG from SQS monitor to API monitor

---
+++ ils_middleware/dags/api_monitor.py
@@ -0,0 +1,89 @@
+import json
+import logging
+
+import httpx
+
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task
+from airflow.providers.standard.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.sdk import get_current_context
+
+from ils_middleware.dags.alma import institutions
+
+from ils_middleware.tasks.general import (
+    get_resource,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def _parse_institutional_msgs(messages: list) -> list:
+    institutional_messages = []
+    all_institutions = institutions + ["stanford", "cornell"]
+    for message in messages:
+        if message["group"].casefold() in all_institutions:
+            institutional_messages.append(message)
+    return institutional_messages
+
+
+def _trigger_dags(**kwargs):
+    messages = kwargs.get("messages")
+    for message in messages:
+        logger.info(f"Trigger DAG for {message['group']}")
+        # Assumes the DAG name is the same as the group
+        group = message["group"].casefold()
+        TriggerDagRunOperator(
+            task_id=f"{group}-dag-run",
+            trigger_dag_id=group,
+            conf={"message": message},
+        ).execute(kwargs)
+
+
+@dag(
+    start_date=datetime(2024, 1, 15),
+    schedule=timedelta(minutes=5),
+    catchup=False,
+)
+def monitor_institutions_messages():
+    """
+    ### Triggers Institutional DAGs based on incoming API call
+    """
+
+    @task
+    def incoming_api_call(*args, **kwargs):
+        context = get_current_context()
+        params = context.get("params")
+        if params is None:
+            raise ValueError("No parameters found in context")
+        resource_payload = get_resource(params["resource"])
+
+        return {
+            "messages": [
+                {
+                    "resource": {
+                        "uri": resource_payload["uri"],
+                        "rdf": resource_payload["data"],
+                    },
+                    "group": params["group"],
+                    "user": params["user"],
+                    "id": resource_payload["uuid"],
+                }
+            ]
+        }
+
+    @task
+    def parse_messages(messages: list) -> list:
+        return _parse_institutional_msgs(messages)
+
+    @task
+    def trigger_institutional_dags(**kwargs):
+        _trigger_dags(**kwargs)
+
+    messages = incoming_api_call()
+    group_messages = parse_messages(messages)
+
+    trigger_institutional_dags(messages=group_messages)
+
+
+monitor_export_messages = monitor_institutions_messages()
