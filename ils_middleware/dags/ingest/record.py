86e2a5d1203238a74dc3ae1cea3a180d5f44e4d3
Jeremy Nelson <jermnelson@gmail.com>
2025-02-24 15:56:44 -0800

    feat: Load and parse http and file system record tasks

--- ils_middleware/dags/ingest/record.py
+++ ils_middleware/dags/ingest/record.py
@@ -6,6 +6,9 @@ from datetime import datetime
 from airflow.decorators import dag, task
 from airflow.operators.python import get_current_context
 
+from ils_middleware.tasks.amazon.bluecore_records_s3 import get_file
+from ils_middleware.tasks.bluecore.batch import is_zip, parse_file_to_graph
+
 logger = logging.getLogger(__name__)
 
 @dag(schedule_interval=None, 
@@ -24,20 +27,39 @@ def loader():
         if file_path is None:
             raise ValueError("Missing file path")
         logger.info(f"Ingesting {file_path}")
-        return file_path
+        local_file_path = get_file(file=file_path)
+        return local_file_path
 
-    @task()
+    @task.branch(task_id="process_choice")
+    def choose_processing(**kwargs):
+        file_path = kwargs.get("file")
+        if is_zip(file_path):
+            return ['process_zip']
+        return ['process']
+
+    @task(trigger_rule="one_success")
     def process(*args, **kwargs):
         file_path = kwargs.get("file")
         logger.info(f"Processing data {file_path}")
-        return []
+        resources = parse_file_to_graph(file_path)
+        logger.info(f"{len(resources)} Resources to process")
+        return resources
 
-    @task()
+    @task
+    def process_zip(**kwargs):
+        logger.info(f"Process zip file")
+
+    @task(trigger_rule="one_success")
     def store(*args, **kwargs):
         record=kwargs.get("record")
         logger.info(f"Storing data {record}")
 
     file_path = ingest()
+    next_task = choose_processing(file=file_path) 
     records = process(file=file_path)
+    process_zip_task = process_zip(file=file_path)
+
+    next_task >> [records, process_zip_task]
+    process_zip_task >> records
     store.expand(record=records)
 loader()
