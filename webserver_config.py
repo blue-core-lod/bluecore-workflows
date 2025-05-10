4a0d2f426ba1d039dd1b8e1ac7d2e5f5fa227dd4
Jeremy Duncan <jpd294@cornell.edu>
2025-05-10 18:00:53 -0400

    t28: used ruff formatting

--- webserver_config.py
+++ webserver_config.py
@@ -11,7 +11,7 @@ from tokenize import Exponent
 
 from airflow.providers.fab.auth_manager.security_manager.override import (
     AUTH_OAUTH,
-    FabAirflowSecurityManagerOverride
+    FabAirflowSecurityManagerOverride,
 )
 
 from flask_appbuilder import expose
@@ -24,35 +24,37 @@ logger = logging.getLogger(__name__)
 WTF_CSRF_ENABLED = True
 WTF_CSRF_TIME_LIMIT = None
 
-AUTH_ROLE_ADMIN = 'Admin'
+AUTH_ROLE_ADMIN = "Admin"
 AUTH_TYPE = AUTH_OAUTH
 
 AUTH_USER_REGISTRATION = True
 AUTH_USER_REGISTRATION_ROLE = "Public"
 
 AUTH_ROLES_MAPPING = {
-    "airflow_admin":  ["Admin"],
-    "airflow_op":     ["Op"],
-    "airflow_user":   ["User"],
+    "airflow_admin": ["Admin"],
+    "airflow_op": ["Op"],
+    "airflow_user": ["User"],
     "airflow_viewer": ["Viewer"],
     "airflow_public": ["Public"],
 }
 
 # Pull .env constants ==========================================================
-CLIENT_ID             = os.getenv("AIRFLOW_KEYCLOAK_CLIENT_ID")
-CLIENT_SECRET         = os.getenv("AIRFLOW_KEYCLOAK_CLIENT_SECRET")
-REALM                 = os.getenv("AIRFLOW_KEYCLOAK_REALM")
+CLIENT_ID = os.getenv("AIRFLOW_KEYCLOAK_CLIENT_ID")
+CLIENT_SECRET = os.getenv("AIRFLOW_KEYCLOAK_CLIENT_SECRET")
+REALM = os.getenv("AIRFLOW_KEYCLOAK_REALM")
 KEYCLOAK_INTERNAL_URL = os.getenv("KEYCLOAK_INTERNAL_URL")
 KEYCLOAK_EXTERNAL_URL = os.getenv("KEYCLOAK_EXTERNAL_URL")
 
 # build the OIDC URLs ==========================================================
-PROVIDER_NAME          = "keycloak"
-OIDC_ISSUER_INTERNAL   = f"{KEYCLOAK_INTERNAL_URL}realms/{REALM}" # For airflow-to-keycloak communication (httpx, etc.)
-OIDC_ISSUER_EXTERNAL   = f"{KEYCLOAK_EXTERNAL_URL}realms/{REALM}" # For redirect_uri in browser
+PROVIDER_NAME = "keycloak"
+OIDC_ISSUER_INTERNAL = f"{KEYCLOAK_INTERNAL_URL}realms/{REALM}"  # For airflow-to-keycloak communication (httpx, etc.)
+OIDC_ISSUER_EXTERNAL = (
+    f"{KEYCLOAK_EXTERNAL_URL}realms/{REALM}"  # For redirect_uri in browser
+)
 OIDC_BASE_EXTERNAL_URL = f"{OIDC_ISSUER_EXTERNAL}/protocol/openid-connect"
 OIDC_BASE_INTERNAL_URL = f"{OIDC_ISSUER_INTERNAL}/protocol/openid-connect"
-OIDC_TOKEN_URL         = f"{OIDC_BASE_INTERNAL_URL}/token"
-OIDC_AUTH_URL          = f"{OIDC_BASE_EXTERNAL_URL}/auth"
+OIDC_TOKEN_URL = f"{OIDC_BASE_INTERNAL_URL}/token"
+OIDC_AUTH_URL = f"{OIDC_BASE_EXTERNAL_URL}/auth"
 
 # Debug lines to ensure proper env variables are set and urls are correct
 print("")
@@ -79,11 +81,9 @@ OAUTH_PROVIDERS = [
             "request_token_url": None,
             "client_id": CLIENT_ID,
             "client_secret": CLIENT_SECRET,
-            "client_kwargs": {
-                "scope": "email profile"
-            }
-        }
-    }        
+            "client_kwargs": {"scope": "email profile"},
+        },
+    }
 ]
 
 req = httpx.get(OIDC_ISSUER_INTERNAL)
@@ -98,17 +98,14 @@ key_der_base64 = req.json()["public_key"]
 key_def = b64decode(key_der_base64)
 public_key = serialization.load_der_public_key(key_def)
 
-class CustomSecurityManager(FabAirflowSecurityManagerOverride):
 
+class CustomSecurityManager(FabAirflowSecurityManagerOverride):
     def oauth_user_info(self, provider, response):
         user_info = {}
         if provider == PROVIDER_NAME:
             token = response.get("access_token")
             decoded_jwt = jwt.decode(
-                token,
-                public_key,
-                algorithms=["HS256", "RS256"],
-                audience=CLIENT_ID
+                token, public_key, algorithms=["HS256", "RS256"], audience=CLIENT_ID
             )
             roles = decoded_jwt["resource_access"][CLIENT_ID]["roles"]
             if not roles:
@@ -118,9 +115,10 @@ class CustomSecurityManager(FabAirflowSecurityManagerOverride):
             user_info["username"] = decoded_jwt.get("preferred_username")
             user_info["email"] = decoded_jwt.get("email")
             user_info["first_name"] = decoded_jwt.get("given_name")
-            user_info["last_name"] =  decoded_jwt.get("family_name")
+            user_info["last_name"] = decoded_jwt.get("family_name")
             user_info["role_keys"] = roles
             logger.info(f"User is logging in with info: {user_info}")
         return user_info
-    
+
+
 SECURITY_MANAGER_CLASS = CustomSecurityManager
