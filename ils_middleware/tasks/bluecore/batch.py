34d52955ce234dda154a7f7e6ad27a26a5ab0ee3
Ed Summers <ehs@pobox.com>
2025-10-21 05:32:27 -0400

    Use save_graph for persisting to the database
    Use the new `save_graph()` function in bluecore_models for persisting a
    rdflib Graph to the databae. This will handle updating resources as
    needed. Testing of this functionality lives over in the bluecore-models repository.
    
    Note: setting `AIRFLOW_VAR_BLUECORE_URL` is needed for setting the default
    Bluecore namespace. For development, the namespace http://localhost:3000
    is used since it is assumed you are running bluecore_api on port 3000
    in development. In production we will be using `http://dev.bcld.info".
    
    Closes #67

--- ils_middleware/tasks/bluecore/batch.py
+++ ils_middleware/tasks/bluecore/batch.py
@@ -1,53 +1,13 @@
-import json
 import logging
 import os
 import pathlib
-import rdflib
-from typing import Any, Dict, List
-
-from bluecore_models.utils.graph import (
-    BF,
-    generate_entity_graph,
-    generate_other_resources,
-    init_graph,
-    handle_external_subject,
-)
 
 BLUECORE_URL = os.environ.get("BLUECORE_URL", "https://bcld.info/")
 
 logger = logging.getLogger(__name__)
 
 
-def _add_other_resources(**kwargs):
-    """
-    Adds Other Resources from the entity graph to the resource list
-    """
-    file_graph: rdflib.Graph = kwargs["file_graph"]
-    entity_graph_dict: Dict[str, Any] = kwargs["entity_graph"]
-    entity: str = kwargs["entity"]
-    resources: List[Dict[str, Any]] = kwargs["resources"]
-    entity_graph_str = json.dumps(entity_graph_dict)
-    entity_graph: rdflib.Graph = rdflib.Graph().parse(
-        data=entity_graph_str, format="json-ld"
-    )
-    entity = rdflib.URIRef(entity)
-    other_resources: List[Dict[str, str]] = generate_other_resources(
-        file_graph, entity_graph
-    )
-    for resource in other_resources:
-        # This data for other_resources is not framed.
-        new_resource: Dict[str, Any] = json.loads(resource["graph"])
-        resources.append(
-            {
-                "class": "OtherResource",
-                "uri": resource["uri"],
-                "resource": new_resource,
-                "bibframe_resource_uri": str(entity),
-            }
-        )
-
-
-def delete_upload(upload: str, remove_empty_parent: bool = False):
+def delete_upload(upload: str, remove_empty_parent: bool = False) -> None:
     """
     Deletes upload file
     """
@@ -64,67 +24,3 @@ def is_zip(file_name: str) -> bool:
     if file_name.endswith(".zip") or file_name.endswith(".gz"):
         return True
     return False
-
-
-def parse_file_to_graph(file_str: str) -> List[Dict[str, Any]]:
-    file_path = pathlib.Path(file_str)
-    if not file_path.exists():
-        raise ValueError(f"{file_path} does not exist or Airflow cannot read")
-    resources: List[Dict[str, Any]] = []
-    file_graph = init_graph()
-    file_graph.parse(data=file_path.read_text(), format="json-ld")
-    works: dict = {}
-    for work in file_graph.subjects(predicate=rdflib.RDF.type, object=BF.Work):
-        if isinstance(work, rdflib.BNode):
-            logger.info(f"Work {work} is a blank node, not processing")
-            continue
-        work_graph = generate_entity_graph(file_graph, work)
-        updated_payload: Dict[str, Any] = handle_external_subject(
-            data=work_graph.serialize(format="json-ld"),
-            type="works",
-            bluecore_base_url=BLUECORE_URL,
-        )
-        works[str(work)] = updated_payload["uri"]
-        resources.append(
-            {
-                "class": "Work",
-                "uri": updated_payload["uri"],
-                "uuid": updated_payload["uuid"],
-                "resource": updated_payload["data"],
-            }
-        )
-        _add_other_resources(
-            resources=resources,
-            file_graph=file_graph,
-            entity_graph=updated_payload["data"],
-            entity=updated_payload["uri"],
-        )
-    for instance in file_graph.subjects(predicate=rdflib.RDF.type, object=BF.Instance):
-        if isinstance(instance, rdflib.BNode):
-            logger.info(f"Instance {instance} is a blank node, not processing")
-            continue
-        instance_graph = generate_entity_graph(file_graph, instance)
-        updated_payload = handle_external_subject(
-            data=instance_graph.serialize(format="json-ld"),
-            type="instances",
-            bluecore_base_url=BLUECORE_URL,
-        )
-        instance_payload: Dict[str, Any] = {
-            "class": "Instance",
-            "uri": updated_payload["uri"],
-            "uuid": updated_payload["uuid"],
-            "resource": updated_payload["data"],
-        }
-        instance_of_work = instance_graph.value(
-            subject=instance, predicate=BF.instanceOf
-        )
-        if instance_of_work is not None and str(instance_of_work) in works:
-            instance_payload["work_uri"] = works.get(str(instance_of_work))
-        resources.append(instance_payload)
-        _add_other_resources(
-            resources=resources,
-            file_graph=file_graph,
-            entity_graph=updated_payload["data"],
-            entity=updated_payload["uri"],
-        )
-    return resources
--- ils_middleware/tasks/bluecore/storage.py
+++ ils_middleware/tasks/bluecore/storage.py
@@ -1,55 +1,10 @@
 import logging
 
-from airflow.decorators import task
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 logger = logging.getLogger(__name__)
 
 
-def get_bluecore_db():
+def get_bluecore_db() -> str:
     pg_hook = PostgresHook("bluecore_db")
     return str(pg_hook.sqlalchemy_url)
-
-
-@task.virtualenv(
-    requirements=["bluecore-models"],
-    system_site_packages=False,
-)
-def load_file(file_path: str, user_uid: str, bluecore_db: str) -> int:
-    import logging
-
-    logger = logging.getLogger(__name__)
-    if not logger.handlers:
-        logging.basicConfig(level=logging.INFO)
-    """Stores Work or Instance in the Blue Core Database
-
-    Note: Virtualenv is needed because bluecore.models uses SQLAlchemy 2.+
-    and Airflow uses a 1.x version of SQLAlchemy
-    """
-    try:
-        """
-        Set CURRENT_USER_ID from DAG-provided user_uid so #add_version can write
-        versions.keycloak_user_id during ORM events triggered by inserts/updates.
-        """
-        from bluecore_models.models.version import CURRENT_USER_ID
-
-        CURRENT_USER_ID.set(user_uid)
-        logger.info("Using CURRENT_USER_ID: %s", user_uid)
-    except Exception as e:
-        logger.error("Failed to set CURRENT_USER_ID: %s", e)
-
-    import rdflib
-    from sqlalchemy import create_engine
-    from bluecore_models.bluecore_graph import save_graph
-
-    # parse the RDF and give it to the BluecoreGraph
-    graph = rdflib.Graph()
-    graph.parse(file_path)
-
-    # create the database engine
-    engine = create_engine(bluecore_db)
-
-    # save the graph and return the number of triples processed
-    bc_graph = save_graph(engine, graph)
-    return len(bc_graph)
-
