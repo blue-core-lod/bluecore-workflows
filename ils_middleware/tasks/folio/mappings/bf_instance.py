10cf1adc37bee41188154c4bc67a2af692696f2d
Joshua Greben <jgreben@stanford.edu>
2026-06-24 13:15:18 -0700

    Fix sparql queries to retrieve publication data and remove duplicating primary contributor as added contributor

--- ils_middleware/tasks/folio/mappings/bf_instance.py
+++ ils_middleware/tasks/folio/mappings/bf_instance.py
@@ -95,29 +95,24 @@ WHERE {{
     <{bf_instance}> bf:extent ?extent_bnode .
     ?extent_bnode a bf:Extent .
     ?extent_bnode rdfs:label ?extent .
-    <{bf_instance}> bf:dimensions ?dimensions .
+    OPTIONAL {{
+        <{bf_instance}> bf:dimensions ?dimensions .
+    }}
 }}
 """
 
 publication = """PREFIX bf: <http://id.loc.gov/ontologies/bibframe/>
+PREFIX bflc: <http://id.loc.gov/ontologies/bflc/>
 PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 
 SELECT ?publisher ?date ?place
 WHERE {{
    <{bf_instance}> a bf:Instance .
    <{bf_instance}> bf:provisionActivity ?activity .
    ?activity a bf:Publication .
-   ?activity bf:agent ?agent .
-   ?agent a bf:Agent .
-   ?agent rdfs:label ?publisher .
-   OPTIONAL {{
-      ?activity bf:date ?date .
-   }}
-   OPTIONAL {{
-      ?activity bf:place ?place_holder .
-      ?place_holder rdfs:label ?place .
-   }}
+   ?activity bflc:simpleAgent ?publisher .
+   OPTIONAL {{ ?activity bflc:simpleDate ?date . }}
+   OPTIONAL {{ ?activity bflc:simplePlace ?place . }}
 }}
 """
 
