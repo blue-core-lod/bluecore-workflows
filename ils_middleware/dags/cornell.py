4fa61d20ab92301e5c62f32703fc709ce7150037
Aaron Collier <aaron.collier@stanford.edu>
2026-08-28 15:50:15 -0700

    Remove update_sinopia from Stanford and Cornell dags

--- ils_middleware/dags/cornell.py
+++ ils_middleware/dags/cornell.py
@@ -16,10 +16,7 @@ from ils_middleware.tasks.folio.new import post_folio_records
 from ils_middleware.tasks.general import message_from_context, parse_messages
 from ils_middleware.tasks.sinopia.email import (
     notify_and_log,
-    send_update_success_emails,
 )
-from ils_middleware.tasks.sinopia.local_metadata import new_local_admin_metadata
-from ils_middleware.tasks.sinopia.login import sinopia_login
 
 
 def task_failure_callback(ctx_dict) -> None:
@@ -93,37 +90,6 @@ with DAG(
         },
     )
 
-    with TaskGroup(group_id="update_sinopia") as sinopia_update_group:
-        # Sinopia Login
-        login_sinopia = PythonOperator(
-            task_id="sinopia-login",
-            python_callable=sinopia_login,
-            op_kwargs={},
-        )
-
-        # Adds localAdminMetadata
-        local_admin_metadata = PythonOperator(
-            task_id="sinopia-new-metadata",
-            python_callable=new_local_admin_metadata,
-            op_kwargs={
-                "jwt": "{{ task_instance.xcom_pull(task_ids='update_sinopia.sinopia-login', key='return_value') }}",
-                "ils_tasks": {
-                    "FOLIO": [
-                        "new-or-upsert-folio-records",
-                    ]
-                },
-            },
-        )
-
-        login_sinopia >> local_admin_metadata
-
-    notify_sinopia_updated = PythonOperator(
-        task_id="sinopia_update_success_notification",
-        dag=dag,
-        trigger_rule="none_failed",
-        python_callable=send_update_success_emails,
-    )
-
     # Empty Operators
     messages_received = EmptyOperator(task_id="messages_received", dag=dag)
     processing_complete = EmptyOperator(task_id="processing_complete", dag=dag)
@@ -135,6 +101,4 @@ with DAG(
 get_messages >> messages_received >> process_message
 process_message >> bf_graphs >> folio_map_task_group
 folio_map_task_group >> folio_records >> new_folio_records
-new_folio_records >> processed_sinopia >> sinopia_update_group
-sinopia_update_group >> notify_sinopia_updated
-notify_sinopia_updated >> processing_complete
+new_folio_records >> processed_sinopia >> processing_complete
